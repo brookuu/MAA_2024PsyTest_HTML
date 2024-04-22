b34cc08f428e08483234c75641d718cf9fdb61b5
brookuu <69624262+brookuu@users.noreply.github.com>
2024-04-22 20:46:50 +0800

    Add files via upload

---
+++ MAAHTML/preload.js
@@ -0,0 +1,119 @@
+function preloadImages() {
+    let images = [
+        'image/Cover/Background/op1.png',
+        'image/Cover/Background/op2.png',
+        'image/Cover/Background/op3.png',
+        'image/Cover/Background/op4.png',
+        'image/Cover/Background/op5.png'
+    ];
+
+    for (let i = 0; i < images.length; i++) {
+        let img = new Image();
+        img.src = images[i];
+    }
+}
+
+function preloadImages2() {
+    let images = [
+        'image/Illustration1/Background/allert_1.png',
+        'image/Illustration1/Background/allert_2.png',
+        'image/Illustration1/Background/allert_3.png',
+        'image/Illustration1/Background/allert_4.png',
+        'image/Illustration1/Background/allert_5.png',
+        'image/Illustration1/Base_map/ill_1.png',
+        'image/Illustration1/Base_map/ill_2.png',
+        'image/Illustration1/Base_map/ill_3.png',
+        'image/Illustration1/Base_map/ill_4.png',
+        'image/Illustration1/Base_map/ill_5.png'
+    ];
+
+    for (let i = 0; i < images.length; i++) {
+        let img = new Image();
+        img.src = images[i];
+    }
+}
+
+function preloadImages3() {
+    let images = [
+        'image/Quiz/Background/Quiz1.png',
+        'image/Quiz/Background/Quiz2.png',
+        'image/Quiz/Background/Quiz3.png',
+        'image/Quiz/Background/Quiz4.png',
+        'image/Quiz/Background/Quiz5.png',
+    ];
+
+    for (let i = 0; i < images.length; i++) {
+        let img = new Image();
+        img.src = images[i];
+    }
+}
+
+function preloadImages4() {
+    let images = [
+        'image/Illustration2/Background/bg1.png',
+        'image/Illustration2/Background/bg2.png',
+        'image/Illustration2/Background/bg3.png',
+        'image/Illustration2/Background/bg4.png',
+        'image/Illustration2/Background/bg5.png',
+        'image/Illustration2/Negative_film/film1.png',
+        'image/Illustration2/Negative_film/film2.png',
+        'image/Illustration2/Negative_film/film3.png',
+        'image/Illustration2/Negative_film/film4.png',
+        'image/Illustration2/Negative_film/film5.png',
+        'image/Illustration2/Picture/pic1.png',
+        'image/Illustration2/Picture/pic2.png',
+        'image/Illustration2/Picture/pic3.png',
+        'image/Illustration2/Picture/pic4.png',
+        'image/Illustration2/Picture/pic5.png',
+        'image/Illustration2/Picture/pic6.png',
+    ];
+
+    for (let i = 0; i < images.length; i++) {
+        let img = new Image();
+        img.src = images[i];
+    }
+}
+function preloadImages5() {
+    let images = [
+        'image/Quiz_results1/Background/Quiz_r_bg_1.png',
+        'image/Quiz_results1/Background/Quiz_r_bg_2.png',
+        'image/Quiz_results1/Background/Quiz_r_bg_3.png',
+        'image/Quiz_results1/Background/Quiz_r_bg_4.png',
+        'image/Quiz_results1/Background/Quiz_r_bg_5.png',
+        'image/Quiz_results1/Base_map/Quiz_r_bg_1.png',
+        'image/Quiz_results1/Base_map/Quiz_r_bg_2.png',
+        'image/Quiz_results1/Base_map/Quiz_r_bg_3.png',
+        'image/Quiz_results1/Base_map/Quiz_r_bg_4.png',
+        'image/Quiz_results1/Base_map/Quiz_r_bg_5.png',
+        'image/Quiz_results1/All/0.png',
+        'image/Quiz_results1/All/1.png',
+        'image/Quiz_results1/All/2.png',
+        'image/Quiz_results1/All/3.png',
+        'image/Quiz_results1/All/4.png',
+    ];
+
+    for (let i = 0; i < images.length; i++) {
+        let img = new Image();
+        img.src = images[i];
+    }
+}
+
+function preloadImages6() {
+    let images = [
+        'image/Quiz_results2/Background/Quiz_results1.png',
+        'image/Quiz_results2/Background/Quiz_results2.png',
+        'image/Quiz_results2/Background/Quiz_results3.png',
+        'image/Quiz_results2/Background/Quiz_results4.png',
+        'image/Quiz_results2/Background/Quiz_results5.png',
+        'image/Quiz_results2/All/0.png',
+        'image/Quiz_results2/All/1.png',
+        'image/Quiz_results2/All/2.png',
+        'image/Quiz_results2/All/3.png',
+        'image/Quiz_results2/All/4.png',
+    ];
+
+    for (let i = 0; i < images.length; i++) {
+        let img = new Image();
+        img.src = images[i];
+    }
+}
--- MAAHTML/script.js
+++ MAAHTML/script.js
@@ -1,11 +1,10 @@
-document.getElementById("submitBtn").addEventListener("click", function() {
-    var answer1 = parseInt(document.querySelector('input[name="question1"]:checked').value);
-    var answer2 = parseInt(document.querySelector('input[name="question2"]:checked').value);
-    var totalScore = answer1 + answer2;
 
-    if (totalScore >= 1) {
-        alert("你是一個外向的人！");
-    } else {
-        alert("你是一個內向的人！");
-    }
-});
+function AddValue(valueType,value,pageName) {
+    
+    sessionStorage.setItem(valueType, (parseInt(sessionStorage.getItem(valueType)) || 0) + parseInt(value));
+    alert(valueType+parseInt(sessionStorage.getItem(valueType)));
+    
+
+    var pageURL = pageName + ".html";
+    window.location.href = pageURL;
+}
